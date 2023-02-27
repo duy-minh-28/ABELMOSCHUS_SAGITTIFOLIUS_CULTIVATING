1733f0aa0a7c5289a117ad1f5b786809adee109f
KhanhSky-inator <76033045+KhanhSky-inator@users.noreply.github.com>
2023-02-27 13:58:25 +0700

    Create big_project.ino

---
+++ big_project/big_project.ino
@@ -0,0 +1,12 @@
+#define DHT22_PIN A0
+#define LIGHT_SENSOR_PIN A1
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
